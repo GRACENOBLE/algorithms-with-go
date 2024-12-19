439dfe3bcf500e67b41937055c732b2c0d6e2f08
GRACENOBLE <gracenoble72@gmail.com>
2024-12-19 11:08:02 +0300

    feat(insertion sort) did insertion sort

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GRACENOBLE/algorithms-with-go/sorting"
+)
+
+func main() {
+	fmt.Println(sorting.InsertionSort([]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}))
+}
